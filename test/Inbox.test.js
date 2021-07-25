9a0575c5106236d0b8e9d5fe0411f4923a129b05
MrJovy <jovaughn.smith93@gmail.com>
2021-07-25 17:51:36 -0400

    Deploy Contract; log in terminal

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,21 +5,29 @@ const Web3 = require('web3');
 // const web3 = new Web3(ganache.provider());
 
 // ? Using ganache GUI
-const web3 = new Web3('http://127.0.0.1:7545');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
 
-beforeEach(() => {
+// import abi & bytecode
+const {interface, bytecode} = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
     // get a list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
+    accounts = await web3.eth.getAccounts();
 
     // use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(interface)
+            .deploy({ data: bytecode, arguments: ['Hi there!']})
+            .send({from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox', () => {
-    it('deploys a contract', () => {});
-})
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
+});
 
 // ? Example tests using MOCHA
 // ? Don't forget to add the "test": "mocha" under package.json scripts
@@ -40,12 +48,12 @@ describe('Inbox', () => {
 
 // describe('Car', () => {
 //     it('can park', () => {
-//         // const car = new Car();
+//          const car = new Car();
 //         assert.strictEqual(car.park(), 'stopped');
 //     });
 
 //     it('can drive', () => {
-//         // const car = new Car();
+         // const car = new Car();
 //         assert.strictEqual(car.drive(), 'vroom');
 //     })
 // })
